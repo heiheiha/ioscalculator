d2198e646b0823f55a13ce82fc27366db6f49a59
heiheiha <1137029429@qq.com>
2016-06-22 19:02:43 +0800

    计算器第一次提交
    先画一个简单的计算器，然后给text添加变量x，给各个按钮添加函数，通过写函数设置在点击按钮的时候将数字显示到text窗口中。

--- ioscalculator/ViewController.swift
+++ ioscalculator/ViewController.swift
@@ -10,6 +10,125 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var x: UITextField!
+    @IBAction func h1(sender: AnyObject) {
+       if x.text!.isEmpty
+        {
+            x.text="1"
+        }
+        else{
+            x.text=x.text!+"1"
+            
+        }
+        
+    }
+    @IBAction func h2(sender: AnyObject) {
+        x.text=x.text!+"2"
+    }
+    @IBAction func h3(sender: AnyObject) {
+        if x.text!.isEmpty
+        {
+            x.text="3"
+        }
+        else{
+            x.text=x.text!+"3"
+            
+        }
+    }
+    @IBAction func h4(sender: AnyObject) {
+        if x.text!.isEmpty
+        {
+            x.text="4"
+        }
+        else{
+            x.text=x.text!+"4"
+            
+        }
+    }
+    @IBAction func h5(sender: AnyObject) {
+        if x.text!.isEmpty
+        {
+            x.text="5"
+        }
+        else{
+            x.text=x.text!+"5"
+            
+        }
+    }
+    @IBAction func h6(sender: AnyObject) {
+        if x.text!.isEmpty
+        {
+            x.text="6"
+        }
+        else{
+            x.text=x.text!+"6"
+            
+        }
+    }
+    @IBAction func h7(sender: AnyObject) {
+        if x.text!.isEmpty
+        {
+            x.text="7"
+        }
+        else{
+            x.text=x.text!+"7"
+            
+        }
+    }
+    @IBAction func h8(sender: AnyObject) {
+        if x.text!.isEmpty
+        {
+            x.text="8"
+        }
+        else{
+            x.text=x.text!+"8"
+            
+        }
+    }
+    @IBAction func h9(sender: AnyObject) {
+        if x.text!.isEmpty
+        {
+            x.text="9"
+        }
+        else{
+            x.text=x.text!+"9"
+            
+        }
+    }
+    @IBAction func h0(sender: AnyObject) {
+        if x.text!.isEmpty
+        {
+            x.text="0"
+        }
+        else{
+            x.text=x.text!+"0"
+            
+        }
+    }
+    @IBAction func hdian(sender: AnyObject) {
+        if x.text!.isEmpty
+        {
+            x.text="."
+        }
+        else{
+            x.text=x.text!+"."
+            
+        }
+    }
+    @IBAction func hjia(sender: AnyObject) {
+    }
+    @IBAction func hjian(sender: AnyObject) {
+    }
+    @IBAction func hchen(sender: AnyObject) {
+    }
+    @IBAction func hchu(sender: AnyObject) {
+    }
+    @IBAction func hdengyu(sender: AnyObject) {
+    }
+    @IBAction func hguiling(sender: AnyObject) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
